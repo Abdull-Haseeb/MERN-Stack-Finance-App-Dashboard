a31245d354f04e8f78c598306b7e5b2ccdc732fd
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-11-12 19:56:51 +0500

    Defining Month Schema

--- server/models/KPIs.js
+++ server/models/KPIs.js
@@ -22,3 +22,31 @@ const daySchema = new Schema(
   },
   { toJSON: { getters: true } }
 );
+
+
+const monthSchema = new Schema(
+  {
+    month: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    operationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    nonOperationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
