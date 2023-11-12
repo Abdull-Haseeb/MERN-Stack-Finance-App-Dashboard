75f384fe0f5a0977a4bf0fb0bcc7380ad1805099
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-11-12 19:54:36 +0500

    Defining Schema

--- server/models/KPIs.js
+++ server/models/KPIs.js
@@ -4,3 +4,21 @@ import { loadType } from "mongoose-currency";
 
 const Schema = mongoose.Schema;
 loadType(mongoose);
+
+
+const daySchema = new Schema(
+  {
+    date: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
