a76baa81a113930325340d539f24954f6ca5c645
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-11-16 20:58:35 +0500

    adding prodcut schema pricing

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,3 +4,25 @@ import { loadType } from "mongoose-currency";
 
 const Schema = mongoose.Schema;
 loadType(mongoose);
+
+const ProductSchema = new Schema(
+  {
+    price: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expense: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    transactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+      },
+    ],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
