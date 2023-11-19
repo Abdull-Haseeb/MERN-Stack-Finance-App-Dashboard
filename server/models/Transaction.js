6846aa326e63c3ccc62362359672801111457a28
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-11-19 18:47:19 +0500

    adding schema

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -3,3 +3,25 @@ import { loadType } from "mongoose-currency";
 
 const Schema = mongoose.Schema;
 loadType(mongoose);
+
+
+const TransactionSchema = new Schema(
+  {
+    buyer: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    productIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
